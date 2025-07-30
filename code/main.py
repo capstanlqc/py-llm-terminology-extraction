e62f764f71e9fcc10a6ddc3a1fc4207230a8b2f7
Manuel Souto Pico <m.soutopico@gmail.com>
2025-07-30 12:06:53 +0200

    Other tests... (disabled)

--- code/main.py
+++ code/main.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
 # load the libraries
+
+import os
+import sys
 import openai
-import sys, os
 import IPython
 
 # from langchain.llms import OpenAI
@@ -19,9 +21,8 @@ import requests
 from pypdf import PdfReader
 import markdown
 
-# ############# PROGRAM DESCRIPTION ###########################################
 
-text = "foo"
+# ############# PROGRAM DESCRIPTION ###########################################
 
 # initialize arg parser with a description
 parser = argparse.ArgumentParser(description=text)
@@ -182,3 +183,150 @@ messages = [
 # the text has been limited to the first 1000 characters, not sure waht is the limit but the file.pdf is too long
 response = get_completion(messages, temperature=0)
 print(response)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+exit()
+# other tests... 
+match input_type:
+    case "doc_path":
+        print("doc_path")
+        pass
+    case "doc_url":
+        # https://www.oecd.org/pisa/data/pisa2022technicalreport/PISA-2022-Technical-Report-Ch-7-PISA-Translation.pdf
+        print("doc_url")
+        pass
+    case "omegat_omt":
+        print("omegat_omt")
+        pass
+    case "omegat_git":
+        print("omegat_git")
+        pass
+    case _:  # text
+        print("_")
+        messages = [
+            {"role": "system", "content": prompt},
+            {"role": "user", "content": text},
+        ]
+        print(f"{prompt=}")
+        print(f"{text[0:100]=}")
+        exit()
+        response = get_completion(messages, temperature=0)
+        print(response)
+        with open("data/test5.md", mode="w") as file:
+            file.write(response)
+
+
+sys.exit()
+
+
+# cases
+
+# file: text file -> import directly
+# omegat project: unzip
+# url to git repo: download
+# url to document
+
+
+###################################################################################################
+
+system_instruction = """
+Your trask is to extract all the verbs from the poem provided in the input.
+For each verbal form found, please provide its lemma, its actual form found, and how many times that verb is used.
+Please include also the total number of lemmas and verbal forms found.
+Provide the results as a table. 
+
+For example: 
+
+INPUT
+=====
+
+She tries to do what I try and did achieve.
+
+OUTPUT
+======
+
+|  LEMMA  |    FORMS   | FREQUENCY |
+|:-------:|:----------:|:---------:|
+| try     | tries, try | 2         |
+| do      | do, did    | 2         |
+| achieve | achieve    | 1         |
+
+Total number of lemmas: 3
+Total number of forms: 5
+"""
+
+user_message = """
+Deep into that darkness peering,
+Long I stood there, wondering, fearing,
+Doubting, dreaming dreams no mortals
+Ever dared to dream before;
+But the silence was unbroken,
+And the stillness gave no token,
+And the only word there spoken
+Was the whispered word, "Lenore!"
+This I whispered, and an echo
+Murmured back the word, "Lenore!"
+Merely this, and nothing more.
+"""
+
+messages = [
+    {"role": "system", "content": system_instruction},
+    {"role": "user", "content": user_message},
+]
+
+response = get_completion(messages)
+print(response)
+
+"""
+|  LEMMA   |      FORMS      | FREQUENCY |
+|:--------:|:---------------:|:---------:|
+| peer     | peering         | 1         |
+| stand    | stood           | 1         |
+| wonder   | wondering       | 1         |
+| fear     | fearing         | 1         |
+| doubt    | doubting        | 1         |
+| dream    | dreaming, dream | 2         |
+| dare     | dared           | 1         |
+| be       | was             | 1         |
+| break    | unbroken        | 1         |
+| give     | gave            | 1         |
+| speak    | spoken          | 1         |
+| whisper  | whispered       | 2         |
+| murmur   | murmured        | 1         |
+
+Total number of lemmas: 13
+Total number of forms: 15
+"""
+
+# also https://platform.openai.com/playground/p/WKgxdT9LsORPFl49soIH2kvI?model=gpt-4-turbo&mode=chat
